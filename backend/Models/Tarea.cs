642678b9d151780a0ae45a6f8a60576c1044fa81
Dany Pop <yjoedany@gmail.com>
2019-10-18 00:16:40 -0600

    .

--- backend/Models/Tarea.cs
+++ backend/Models/Tarea.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace backend.Models
 {
+    [Table("tareas")]
     public class Tarea
     {
         [Key]
